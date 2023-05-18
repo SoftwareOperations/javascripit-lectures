36cc8bbae4b3ab75f68b321cd21591ed7a51d3f6
Amit Raut <itspm99@gmail.com>
2023-05-18 21:38:21 +0530

    Lecture1

---
+++ Lecture1/interaction.js
@@ -0,0 +1,8 @@
+let result =10;
+alert(result);
+
+let username = prompt('Entre your name');
+console.log(username);
+
+let isDelete = confirm(' Are you sure you want to delete');
+console.log(isDelet);
---
+++ Lecture1/main.js
@@ -0,0 +1,2 @@
+console.log("Hello Javascripts");
+console.log("again");
